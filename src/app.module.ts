1d9200a01e4aa5ce87a7186753fc3eec7d7a3c0c
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2022-12-31 17:06:00 +0100

    mise a jour de user.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { Users } from './users/entities/user.entity';
 
 @Module({
   imports: [
     UsersModule,
-    ConfigModule.forRoot()],
+    ConfigModule.forRoot()
+    TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [Users],
+        synchronize: true,
+      }),
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, EntityRepository, Repository } from 'typeorm';
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstname: string;
+
+    @Column()
+    lastname: string;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
+    createdAt: Date;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
+    updateAt: Date;
+}
+
+@EntityRepository(Users)
+export class UsersRepository extends Repository<Users> {}
