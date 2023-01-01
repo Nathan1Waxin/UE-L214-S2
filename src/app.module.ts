2f42037315a3654f8dd8b2690c52056f37b378cd
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2023-01-01 14:30:09 +0100

    des changement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,19 +8,19 @@ import { Users } from './users/entities/user.entity';
 
 @Module({
   imports: [
-    UsersModule,
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: 3306,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: [Users],
-        synchronize: true,
-      }),
-    ],
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Users],
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users, UsersRepository } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
+  let usersRepository: Repository<Users>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useValue: {
+            save: jest.fn(),
+            find: jest.fn()
+          },
+        },
+        UsersRepository
+      ]
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = app.get<UsersController>(UsersController);
+    usersService = app.get<UsersService>(UsersService);
+    usersRepository = app.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+    expect(usersService).toBeDefined();
+    expect(usersRepository).toBeDefined();
   });
+
+  describe('Find all Users', () => {
+    it('should return an array of users', async () => {
+      const data = usersController.findAll();
+      expect(usersController.findAll()).toBe(data);
+      console.log('Test Controller : Should find all users => 200');
+    });
+  });
+
 });
